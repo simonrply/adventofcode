d63833a9820d0090bfc0539ac6d1b12f60cd68be
simon rapilly <simon.rapilly@utt.fr>
2016-12-08 16:55:53 +0100

    day4

--- day4/day4.hs
+++ day4/day4.hs
@@ -2,9 +2,29 @@ module Main (main) where
 import qualified Data.Map as Map
 import Data.Char
 import Data.List
+import Data.Maybe
+
+data Room = Room String Int String deriving (Show)
 
 
-data Room = Room String Int String
+lineToRoom :: String -> Room
+lineToRoom input = Room name sectorId checksum
+    where 
+        indexCheck = fromJust $ elemIndex '[' input
+        sectorId = read $ drop (indexCheck - 3) $ take indexCheck input :: Int 
+        checksum = init $ drop (indexCheck + 1) input
+        name = foldl (\list char -> case char of
+            '-' -> list
+            _ -> char : list)  "" (take (indexCheck - 4) input)
+
+
+linesToRoomList :: String -> [Room]
+linesToRoomList input = map lineToRoom (lines input)
+
+solution :: [Room] -> Int
+solution list = foldl (\x (Room name roomId checksum) -> case (real name checksum) of
+    True -> x + roomId
+    False -> x) 0 list
  
 mostCommon :: String -> Map.Map Char Int
 mostCommon name = foldl (\list char -> Map.insertWith (+)  char 1 list) Map.empty name
@@ -15,11 +35,14 @@ mostCommonFormated input = foldl (\list element -> list ++ [fst element] ) [] (t
             list = Map.toList input
             f (char,number) = - (number * 100 - (ord char)) 
 
-real :: Room -> Bool
-real (Room name _ checksum) = (mostCommonFormated $ mostCommon name) == checksum  
+real :: String -> String -> Bool
+real name checksum = (mostCommonFormated $ mostCommon name) == checksum  
 
   
-
+file = "input.txt"
 
 main :: IO ()
-main = return ()
+main =  do
+    x <-  readFile file
+    putStrLn $ show $ solution $ linesToRoomList  x
+                                
